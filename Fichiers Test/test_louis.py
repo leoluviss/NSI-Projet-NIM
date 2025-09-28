a2f5d8dbee16f153be382a2b94b9ed523162491e
Louis Bertagna <louisbrtgn@gmail.com>
2025-09-28 21:58:09 +0100

    Verification of all the function i did today

--- Fichiers Test/test_louis.py
+++ Fichiers Test/test_louis.py
@@ -20,4 +20,24 @@ def affichage(n, caractere):
     saut_de_ligne(1)
     return
 
-affichage(5, '*')
+def choix_joueur():
+    saut_de_ligne(1)
+    separateur('-')
+    n=0
+    while not (1 <= n <= 3):
+        n = int(input("Nombre d'objets retirés ? "))
+        if n < 1 or n > 3:
+            print("Choisir un nombre d'objets entre 1 et 3.")
+    return n
+
+def prochain_joueur(joueur):
+    assert joueur == 1 or joueur == 2
+    if joueur == 1:
+        return 2
+    else:
+        return 1
+
+assert prochain_joueur(1) == 2
+assert prochain_joueur(2) == 1
+
+print(prochain_joueur(3))
