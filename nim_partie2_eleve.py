30b1641233aeef48c0674d3a721b3a5ceed94f7d
Louis Bertagna <louisbrtgn@gmail.com>
2025-10-02 10:23:02 +0200

    nim_partie2_eleve.py set as the original state

--- nim_partie2_eleve.py
+++ nim_partie2_eleve.py
@@ -88,12 +88,22 @@ assert tous_zeros([0, 0, 0])
 #%% Numération binaire
 
 def binaire_vers_decimale(tab):
+    """
+    Renvoie l'entier naturel
+    dont le tableau de bits est tab
+
+    Parameters:
+    -----------
+        tab : tableau de bits (0 ou 1)
+
+    Returns:
+    -------
+        decimale : int
+    """
     puissance = 1
     decimale = 0
-    for bit in reversed(tab):
-        decimale += bit * puissance
-        puissance *= 2
-    return decimale
+    #à compléter
+
 
 #tests unitaires
 assert binaire_vers_decimale([0]) == 0
@@ -104,15 +114,26 @@ assert binaire_vers_decimale([1, 0, 0]) == 4
 assert binaire_vers_decimale([1, 0, 1]) == 5
 
 def decimale_vers_binaire(n):
+    """
+    Renvoie le tableau de bits
+    de la représentation binaire de n
+    Bits de poids forts à gauche
+
+    Parameters:
+    -----------
+        n: int
+            un entier >=0 en base 10
+            précondition n >= 0
+
+    Returns:
+    --------
+        tableau d'entiers
+    """
+    #précondition
     assert n >= 0
     binaire = []
-    if n == 0:
-        return [0]
-    while n > 0:
-        binaire.append(n % 2)
-        n //= 2
-    binaire.reverse()
-    return binaire
+    #à compléter
+
 
 #tests unitaires
 assert decimale_vers_binaire(0) == [0]
@@ -121,6 +142,7 @@ assert decimale_vers_binaire(2) == [1, 0]
 assert decimale_vers_binaire(3) == [1, 1]
 assert decimale_vers_binaire(4) == [1, 0, 0]
 assert decimale_vers_binaire(5) == [1, 0, 1]
+
 
 #%% Opérateur XOR
 
